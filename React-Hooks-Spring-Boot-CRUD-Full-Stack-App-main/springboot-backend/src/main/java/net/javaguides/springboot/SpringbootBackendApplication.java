bff01940d6d8784edc80fc49a7459a973a9a2c88
haelgh <tytarenko.olha@lll.kpi.ua>
2023-01-08 00:19:23 +0200

    DB's data output on web-page

--- React-Hooks-Spring-Boot-CRUD-Full-Stack-App-main/springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ React-Hooks-Spring-Boot-CRUD-Full-Stack-App-main/springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboot;
 
+import net.javaguides.springboot.model.Kid;
 import net.javaguides.springboot.repository.KidRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +19,18 @@ public class SpringbootBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+//		Kid kid = new Kid();
+//		kid.setFullName("Jane Smith");
+//		kid.setAccount(1236);
+//		kid.setBill(541.8);
+//		kid.setKindergarten(4);
+//		kidRepository.save(kid);
+//
+//		Kid kid1 = new Kid();
+//		kid1.setFullName("Colin Hoover");
+//		kid1.setAccount(5678);
+//		kid1.setBill(235.8);
+//		kid1.setKindergarten(3);
+//		kidRepository.save(kid1);
 	}
 }
